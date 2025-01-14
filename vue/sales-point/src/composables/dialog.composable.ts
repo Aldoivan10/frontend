43711bd112b0235438e79bed8e4cbbf13ac8c215
Aldoivan <aldoivan_palma@hotmail.com>
2025-01-13 21:43:31 -0600

    Refactor: Only return 1 object

--- vue/sales-point/src/composables/dialog.composable.ts
+++ vue/sales-point/src/composables/dialog.composable.ts
@@ -1,12 +1,10 @@
 import { DialogSymbol } from '@/plugins/dialog.plugin'
 import { Dialog } from '@/service/dialog.svc'
-import { inject, ref } from 'vue'
+import { inject } from 'vue'
 
 export function useDialog() {
     const dialog = inject<Dialog>(DialogSymbol)
     if (!dialog)
         throw new Error('useDialog debe ser utilizado después de instalar el dialogPlugin.')
-    const $dialog = ref<Dialog>(dialog)
-
-    return { dialog, $dialog: $dialog! }
+    return dialog
 }
