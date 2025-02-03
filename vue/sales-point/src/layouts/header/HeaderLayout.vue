f49ce55836d95b4a9679444446c34f7e21b42fe5
Aldoivan <aldoivan_palma@hotmail.com>
2025-02-03 10:30:41 -0600

    Remove: Unused component props

--- vue/sales-point/src/layouts/header/HeaderLayout.vue
+++ vue/sales-point/src/layouts/header/HeaderLayout.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
-import AuthButton from '@/layouts/header/components/AuthButton.vue'
-import SwitchTheme from '@/layouts/header/components/SwitchTheme.vue'
-import UserTab from '@/layouts/header/components/UserTabs.vue'
-import { useTabStore } from '@/stores/tab.store'
-import { storeToRefs } from 'pinia'
-import AddTabBtn from './components/AddTabBtn.vue'
-import HamburguerBtn from './components/HamburguerBtn.vue'
-
-const tabStore = useTabStore()
-const { tabs } = storeToRefs(tabStore)
+import AuthButton from '@/layouts/header/components/AuthButton.vue';
+import SwitchTheme from '@/layouts/header/components/SwitchTheme.vue';
+import AddTabBtn from './components/AddTabBtn.vue';
+import HamburguerBtn from './components/HamburguerBtn.vue';
+import UserTabs from './components/UserTabs.vue';
 </script>
 
 <template>
     <header class="bg-primary-darken-1 pa-2 d-flex ga-1 align-center rounded-0 v-card elevation-4">
         <HamburguerBtn />
-        <AddTabBtn :tabs />
-        <UserTab :tabs />
+        <AddTabBtn />
+        <UserTabs />
         <SwitchTheme />
         <AuthButton />
     </header>
