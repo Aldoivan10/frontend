df9be0cdb5dd59c85cd11ca437c4e1fdc072d3c7
Aldoivan <aldoivan_palma@hotmail.com>
2025-02-05 22:26:16 -0600

    Created: Search input for tables

---
+++ vue/sales-point/src/components/SearchInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const $emit = defineEmits<{ search: [val: string]; focusTable: void }>()
+
+function onEnter(evt: KeyboardEvent) {
+    const $input = evt.target as HTMLInputElement
+    $emit('search', $input.value)
+}
+</script>
+
+<template>
+    <v-text-field
+        @keyup.down="$emit('focusTable')"
+        @keyup.enter="onEnter"
+        variant="outlined"
+        label="Buscar"
+        color="white"
+        class="my-2"
+        hide-details
+    />
+</template>
